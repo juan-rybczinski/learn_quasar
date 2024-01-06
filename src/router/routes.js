9b949eba5b6d109cd50469ada2fbf3161b1b3202
juan-rybczinski <jhreplay.lee@gmail.com>
2024-01-07 08:11:01 +0900

    Typography - 글꼴 활용하기

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,19 +1,22 @@
-
 const routes = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
-    ]
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      {
+        path: 'typography',
+        component: () => import('pages/TypographyPage.vue'),
+      },
+    ],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
     path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
-]
+    component: () => import('pages/ErrorNotFound.vue'),
+  },
+];
 
-export default routes
+export default routes;
